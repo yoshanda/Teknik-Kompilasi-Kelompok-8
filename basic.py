ae08b22565b8a706bb65d25f4f0d375f2e0637a0
yoshanda <yoshanda95@gmail.com>
2022-04-18 12:47:19 +0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -95,25 +95,25 @@ class Position:
 # TOKENS
 #######################################
 
-TT_INT				= 'INT'
+TT_INT		= 'INT'
 TT_FLOAT    	= 'FLOAT'
 TT_IDENTIFIER	= 'IDENTIFIER'
-TT_KEYWORD		= 'KEYWORD'
+TT_KEYWORD	= 'KEYWORD'
 TT_PLUS     	= 'PLUS'
 TT_MINUS    	= 'MINUS'
 TT_MUL      	= 'MUL'
 TT_DIV      	= 'DIV'
-TT_POW				= 'POW'
-TT_EQ					= 'EQ'
+TT_POW		= 'POW'
+TT_EQ		= 'EQ'
 TT_LPAREN   	= 'LPAREN'
 TT_RPAREN   	= 'RPAREN'
-TT_EE					= 'EE'
-TT_NE					= 'NE'
-TT_LT					= 'LT'
-TT_GT					= 'GT'
-TT_LTE				= 'LTE'
-TT_GTE				= 'GTE'
-TT_EOF				= 'EOF'
+TT_EE		= 'EE'
+TT_NE		= 'NE'
+TT_LT		= 'LT'
+TT_GT		= 'GT'
+TT_LTE		= 'LTE'
+TT_GTE		= 'GTE'
+TT_EOF		= 'EOF'
 
 KEYWORDS = [
 	'VAR',
